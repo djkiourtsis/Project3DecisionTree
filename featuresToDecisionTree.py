f3aa08190e70ecb96b7655dc68816d2ff9a119be
djkiourtsis <djkiourtsis@wpi.edu>
2016-09-18 16:37:44 -0400

    Update decision tree class
    This update modifies the decision tree class and functions.

--- featuresToDecisionTree.py
+++ featuresToDecisionTree.py
@@ -35,28 +35,67 @@ def main(argv = None):
 
 class decisionTree:
     parentNode = None
-    childNodes = [0]
+    childNodes = []
+    childSplitValues = []
     splitFeatureID = 0
-    prevFeatureIDs = None
+    prevFeatureIDs = []
+    remainingFeatureIDs = []
     treeTrainingData = None
     baseCaseOutput = 0
+    numFeatures = 5
+    treeEntropy = 0.0
     # stuff
-    def __init__(self, parentNode, prevFeatureIDs, treeTrainingData):
+    def __init__(self, parentNode, prevFeatureIDs, remainingFeatureIDs, treeTrainingData):
         self.parentNode = parentNode
         self.prevFeatureIDs = prevFeatureIDs
+        self.remainingFeatureIDs = remainingFeatureIDs
         self.treeTrainingData = treeTrainingData
+        self.numFeatures = len(prevFeatureIDs) + len(remainingFeatureIDs)
+        if (parentNode == None):
+            # Get entropy for table outcome
+            self.treeEntropy = binaryEntropy(1)
         self.genChildren()
     #stuff
     def genChildren(self):
         # determine if base case (all data classified).  Set baseCaseOutput if so and return.
+        if (len(self.remainingFeatureIDs) < 1): # No more features to split on
+            self.baseCaseOutput = self.treeTrainingData[0][42]
+            return
         # determine best attribute to split on
+        infoGains = [0 for x in xrange(len(self.remainingFeatureIDs))]
+        featureValues = []
+        for x in xrange(len(self.remainingFeatureIDs)):
+            # featureData is a list of feature values and result values for the training data
+            featureData = [[],[]]
+            featureValues.append([])
+            for y in xrange(len(self.treeTrainingData)):
+                featureData[0].append(self.treeTrainingData[y][43+self.remainingFeatureIDs[x]])
+                featureData[1].append(self.treeTrainingData[y][42])
+                if (not self.treeTrainingData[y][43+self.remainingFeatureIDs[x]] in featureValues[x]):
+                    featureValues[x].append(self.treeTrainingData[y][43+self.remainingFeatureIDs[x]])
+            infoGains[x] = gain(featureData, featureValues[x], self.treeEntropy)
         # create children
+        self.splitFeatureID = self.remainingFeatureIDs[0]
+        for x in xrange(1, len(self.remainingFeatureIDs)):
+            for x in xrange(len(featureValues[0])):
+                # Generate child for each possible split of feature
+                # Add child and split feature value to self
+                a = None
         return
     #stuff
     def testData(self, testArray):
         # check if base case.  Return output and prevFeatureIDs if so.
         # pass testData to correct child
         return
+
+
+def gain(dataArray, featureValues, treeEntropy):
+    return 0
+
+
+# Entropy is binary since only possible outcomes are win/loss for player 1
+def binaryEntropy(probability):
+    return 0
 
 
 main(sys.argv)
